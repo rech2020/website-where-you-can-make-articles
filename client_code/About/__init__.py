cb18e829386a8a291078f33809fd7f40764a3ef4
aleshailin2010@gmail.com <aleshailin2010@gmail.com>
2023-11-24 11:02:07 +0000

    Created About and MainServerModule; Deleted Form1, Homepage.ItemTemplate1, Homepage.ItemTemplate2, Homepage.ItemTemplate2.Package1 and 2 more

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate2Template
+from ._anvil_designer import AboutTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,10 +7,20 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Homepage import Homepage
+from ..About import About
 
-class ItemTemplate2(ItemTemplate2Template):
+class About(AboutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def home_link_click(self, **event_args):
+    homepage = Homepage()
+    open_form(homepage)
+
+  def about_link_click(self, **event_args):
+    about = About()
+    open_form(about)
